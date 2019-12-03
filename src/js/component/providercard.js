068df6518b09906936f73dabd7ce9cb808c9c15a
wszmuler <54544111+wszmuler@users.noreply.github.com>
2019-12-02 21:03:16 -0500

    Fix Provider card #5

--- src/js/component/providercard.js
+++ src/js/component/providercard.js
@@ -2,52 +2,63 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import StarRatingComponent from "react-star-rating-component";
+import { Context } from "../store/appContext.js";
 
 import "../../styles/providercard.scss";
 
 export function ProviderCard(props) {
 	return (
-		<div className="">
-			<div className="card profile-card-3">
-				<div className="background-block">
-					<img
-						src="http://gbsoftinc.com/wpgb/wp-content/uploads/2019/11/landscape_service.jpg"
-						//alt="profile-sample1"
-						className="background"
-					/>
-				</div>
-				<div className="profile-thumb-block">
-					<img
-						src={props.providerAvatar.url}
-						//src="http://gbsoftinc.com/wpgb/wp-content/uploads/2019/11/22.jpg"
-						alt="profile-image"
-						className="profile"
-					/>
-				</div>
-				<div className="card-content">
-					<h2>
-						{props.providerCompanyName}
-						<small>{props.providerUserName}</small>
-						<small>{props.providerUserID}</small>
-						<small>Phone: {props.providerPhoneNumber}</small>
-						<small>Email: </small>
-						<small>Website: {props.providerWebsite}</small>
-						<small>
-							<span>
-								Rating:
-								<StarRatingComponent
-									name="rate1"
-									editing={false}
-									starCount={5}
-									value={props.providerRating}
-									starColor="#feb400"
+		<Context.Consumer>
+			{({ store }) => {
+				return (
+					<div className="">
+						<div className="card profile-card-3">
+							<div className="background-block">
+								<img
+									src={
+										store.service[props.index] &&
+										store.service[props.index].acf.serviceBackgroundImg
+									}
+									// src="http://gbsoftinc.com/wpgb/wp-content/uploads/2019/11/landscape_service.jpg"
+									//alt="profile-sample1"
+									className="background"
 								/>
-							</span>
-						</small>
-					</h2>
-				</div>
-			</div>
-		</div>
+							</div>
+							<div className="profile-thumb-block">
+								<img
+									src={props.providerAvatar.url}
+									//src="http://gbsoftinc.com/wpgb/wp-content/uploads/2019/11/22.jpg"
+									alt="profile-image"
+									className="profile"
+								/>
+							</div>
+							<div className="card-content">
+								<h2>
+									{props.providerCompanyName}
+									<small>{props.providerUserName}</small>
+									<small>{props.providerUserID}</small>
+									<small>Phone: {props.providerPhoneNumber}</small>
+									<small>Email: </small>
+									<small>Website: {props.providerWebsite}</small>
+									<small>
+										<span className="d-flex align-items-center justify-content-center">
+											Rating:
+											<StarRatingComponent
+												name="rate1"
+												editing={false}
+												starCount={5}
+												value={props.providerRating}
+												starColor="#feb400"
+											/>
+										</span>
+									</small>
+								</h2>
+							</div>
+						</div>
+					</div>
+				);
+			}}
+		</Context.Consumer>
 	);
 }
 
--- src/js/views/providers.js
+++ src/js/views/providers.js
@@ -2,8 +2,10 @@ import React from "react";
 import { Context } from "../store/appContext.js";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import { ProviderCard } from "../component/providercard.js";
+import PropTypes from "prop-types";
+import { match } from "minimatch";
 
-export const Providers = () => {
+export const Providers = props => {
 	return (
 		<div>
 			<div className="container">
@@ -18,7 +20,7 @@ export const Providers = () => {
 									return (
 										<div className="col-12 col-sm-6 col-lg-4" key={index}>
 											<ProviderCard
-												index={index}
+												index={props.match.params.index}
 												providerCompanyName={provider.acf.providercompanyname}
 												//providerUserName={provider.acf.username}
 												providerPhoneNumber={provider.acf.providerphonenumber}
@@ -39,4 +41,16 @@ export const Providers = () => {
 			</div>
 		</div>
 	);
+};
+
+Providers.propTypes = {
+	providerCompanyName: PropTypes.string,
+	providerUserName: PropTypes.string,
+	providerPhoneNumber: PropTypes.string,
+	providerWebsite: PropTypes.string,
+	providerAvatar: PropTypes.string,
+	providerRating: PropTypes.number,
+	providerUserID: PropTypes.number,
+	index: PropTypes.number,
+	match: PropTypes.object
 };
