e4f0e4d9b645d5f0ca73ab728100c4f637af2af5
gbloncourt <54908400+gbloncourt@users.noreply.github.com>
2019-12-13 08:35:35 -0500

    Updates on 12 13 19
    Updates on 12 13 19

--- src/views/profile.js
+++ src/views/profile.js
@@ -5,28 +5,32 @@ import { Redirect } from "react-router-dom";
 
 import apiConfig from '../utils';
 
-export const Profile = props => {
+class Profile extends React.Component {
 
-    const { params: { index } } = props.match;
+	constructor(props) {
+	  super(props); 
+		  this.state = {
+			  services: [],
+			  serviceEndPoint : (apiConfig.apiUrl+'/service'),
+			  companyName : '',
+			  companyPhone : '',
+			  companyWebsite : ''
+		  }
 
-	// const [providers, setProviders] = useState([]);
+	}
 
-	const [services, setServices] = useState([]);
-	
-	// const [dataLoaded, setdataLoaded] = useState(false);
+    // const { params: { index } } = props.match;
+	// const [services, setServices] = useState([]);
+	// const [serviceEndPoint, setServiceEndPoint] = useState('');
 
-	const [serviceEndPoint, setServiceEndPoint] = useState('');
 
-	// const serviceEndPoint = apiConfig.apiUrl+'/service';
+	// useEffect(() => {
+	// 	setServiceEndPoint(apiConfig.apiUrl+'/service');
+	// 	document.title = 'My Profile';
+	// },[]);
 
-	useEffect(() => {
-		setServiceEndPoint(apiConfig.apiUrl+'/service');
-		document.title = 'My Profile';
-	},[]);
-
-	useEffect(()=>{
-		console.log(`${serviceEndPoint}`);
-		const fetchServices = fetch(`${serviceEndPoint}`)
+	componentDidMount() {
+			const fetchServices = fetch(`${this.state.serviceEndPoint}`)
 			.then(response => {
 				if (!response.ok) {
 					throw Error(response.statusText);
@@ -40,126 +44,159 @@ export const Profile = props => {
 
 			Promise.all([fetchServices])
 			.then(data => {
-				setServices(data[0]);
+				console.log(data[0])
+				this.setState({services : data[0]});
 			})
 			.catch(function(err) {
 				console.log("Error with resolving promises.", err);
 			});
-	},[serviceEndPoint]);
-
-	if ( localStorage.getItem('token') === null	)
-	{
-		return <Redirect to='/login' />
 	}
-	return (
-		<Container >
-		<section className="contact-section my-5">
-			<MDBCard>
-				<MDBRow>
-					<MDBCol lg="12">
-						<MDBCardBody className="form">
-							<h3 className="mt-4">
-								<MDBIcon icon="user-alt" className="pr-2" />
-								User Profile
-							</h3>
-							<MDBRow>
-								<MDBCol md="4">
-									<div className="md-form mb-0 ">
-										<span className="labelprofile">Username: {localStorage.getItem('userName')}</span>
-                                    </div>
-								</MDBCol> 
-								<MDBCol md="4">
-									<div className="md-form mb-0">
-                                    <span className="labelprofile">Name: {localStorage.getItem('userFullName')}</span>
-									</div>
-								</MDBCol>
-                                <MDBCol md="4">
-									<div className="md-form mb-0">
-                                    <span className="labelprofile">Email: {localStorage.getItem('userEmail')}</span>
-									</div>
-								</MDBCol>
-							</MDBRow>
-						</MDBCardBody>
-					</MDBCol>				
-				</MDBRow>				
-				<MDBRow>
-					<MDBCol lg="12">
-						<MDBCardBody className="form">
-							<h3 className="mt-4">
-								<MDBIcon icon="tools" className="pr-2" />
-								Service Provider Information
-							</h3>
-							<MDBRow>
-								<MDBCol md="8">
-									<div className="md-form mb-0">
-                                    <span className="labelprofile">What service do you offer? </span>
-                                        <div className="col-4 align-self-start inputservices">
-											<select className="browser-default custom-select"> 
-												<option value= "0">Choose your service</option>
-												{services && services.map (service => {
-													return (
-														<option key = {service.acf.serviceID} value={service.acf.serviceID}>{service.acf.serviceTitle}</option>
-													);
-													})
-												}
-												{/* <option value="1">LANDSCAPE</option>
-												<option value="2">MAINTENANCE</option>
-												<option value="3">PAINTING</option>
-												<option value="4">PLUMBING</option>
-												<option value="5">REMODELING</option>
-												<option value="6">ROOFING</option> */}
-											</select>
+
+	handleChange = (e) => {
+		const itemName = e.target.name;
+		const itemValue = e.target.value;
+		this.setState({ [itemName]: itemValue });
+	  }
+
+	render() {
+			const { services } = this.state;
+			if ( localStorage.getItem('token') === null	)
+			{
+				return <Redirect to='/login' />
+			}
+			
+			return (
+				<Container >
+				<section className="contact-section my-5">
+					<MDBCard>
+						<MDBRow>
+							<MDBCol lg="12">
+								<MDBCardBody className="form">
+									<h3 className="mt-4">
+										<MDBIcon icon="user-alt" className="pr-2" />
+										User Profile
+									</h3>
+									<MDBRow>
+										<MDBCol md="4">
+											<div className="md-form mb-0 ">
+												<span className="labelprofile">Username: {localStorage.getItem('userName')}</span>
+											</div>
+										</MDBCol> 
+										<MDBCol md="4">
+											<div className="md-form mb-0">
+											<span className="labelprofile">Name: {localStorage.getItem('userFullName')}</span>
+											</div>
+										</MDBCol>
+										<MDBCol md="4">
+											<div className="md-form mb-0">
+											<span className="labelprofile">Email: {localStorage.getItem('userEmail')}</span>
+											</div>
+										</MDBCol>
+									</MDBRow>
+								</MDBCardBody>
+							</MDBCol>				
+						</MDBRow>				
+						<MDBRow>
+							<MDBCol lg="12">
+								<MDBCardBody className="form">
+									<h3 className="mt-4">
+										<MDBIcon icon="tools" className="pr-2" />
+										Service Provider Information
+									</h3>
+									<MDBRow>
+										<MDBCol md="8">
+											<div className="md-form mb-0">
+											<span className="labelprofile">What service do you offer? </span>
+												<div className="col-4 align-self-start inputservices">
+													<select className="browser-default custom-select"> 
+														<option value= "0">Choose your service</option>
+														{services && services.map (service => {
+															return (
+																<option key = {service.acf.serviceID} value={service.acf.serviceID}>{service.acf.serviceTitle}</option>
+															);
+															})
+														}
+														{/* <option value="1">LANDSCAPE</option>
+														<option value="2">MAINTENANCE</option>
+														<option value="3">PAINTING</option>
+														<option value="4">PLUMBING</option>
+														<option value="5">REMODELING</option>
+														<option value="6">ROOFING</option> */}
+													</select>
+												</div>
+											</div>                                   
+										</MDBCol>
+									</MDBRow>
+									<MDBRow>
+										<MDBCol md="6">
+											<div className="md-form mb-0 labelprofile">
+												<MDBInput 
+													type="text" 
+													id="form-contact-company" 
+													label="Company Name" 
+													name="companyName"
+													value={this.state.companyName}
+													onChange={this.handleChange}
+												/>
+											</div>
+										</MDBCol>
+										<MDBCol md="6">
+											<div className="md-form mb-0 labelprofile">
+											<MDBInput 
+												type="text" 
+												id="form-contact-phone" 
+												label="Phone" 
+												name="companyPhone"
+												value={this.state.companyPhone}
+												onChange={this.handleChange} 
+											/>
+											</div>
+										</MDBCol>
+									</MDBRow>
+									<MDBRow>
+										<MDBCol md="6">
+											<div className="md-form mb-0 labelprofile">
+												<MDBInput 
+													type="text" 
+													id="form-contact-website" 
+													label="Website" 
+													name="companyWebsite"
+													value={this.state.companyWebsite}
+													onChange={this.handleChange} 													
+												/>
+											</div>
+										</MDBCol>
+										<MDBCol md="6">
+										<div className="md-form mb-0 labelprofile">
+											<span className="labelprofile">Upload your Avatar/Logo </span>
 										</div>
-									</div>                                   
-								</MDBCol>
-                            </MDBRow>
-							<MDBRow>
-								<MDBCol md="6">
-									<div className="md-form mb-0 labelprofile">
-										<MDBInput type="text" id="form-contact-company" label="Company Name" name="companyName"/>
-									</div>
-								</MDBCol>
-								<MDBCol md="6">
-									<div className="md-form mb-0 labelprofile">
-									<MDBInput type="text" id="form-contact-phone" label="Phone" name="phone"/>
-									</div>
-								</MDBCol>
-							</MDBRow>
-							<MDBRow>
-								<MDBCol md="6">
-									<div className="md-form mb-0 labelprofile">
-										<MDBInput type="text" id="form-contact-website" label="Website" name="website"/>
-									</div>
-								</MDBCol>
-								<MDBCol md="6">
-								<div className="md-form mb-0 labelprofile">
-									<span className="labelprofile">Upload your Avatar/Logo </span>
-								</div>
-								<div className="file-upload-wrapper">
-									<input type="file" id="input-file-now" className="file-upload" />
-								</div>
-								</MDBCol>
-							</MDBRow>	
-							<MDBRow>
-								{/* <MDBCol md="6">
-								<div className="md-form mb-0 labelprofile">
-									<span className="labelprofile">What service do you offer? </span>
-								</div>
-								<div className="file-upload-wrapper">
-									<input type="file" id="input-file-now" className="file-upload" />
-								</div>
-								</MDBCol> */}
-							</MDBRow>
-                            <MDBRow>    
-                                <MDBCol md="10 text-right">
-                                    <MDBBtn color="secondary">Submit</MDBBtn>
-								</MDBCol>
-							</MDBRow>
-						</MDBCardBody>
-					</MDBCol>				
-				</MDBRow>
-			</MDBCard>
-		</section>
-		</Container>	
-	);
-};
+										<div className="file-upload-wrapper">
+											<input type="file" id="input-file-now" className="file-upload" />
+										</div>
+										</MDBCol>
+									</MDBRow>	
+									<MDBRow>
+										{/* <MDBCol md="6">
+										<div className="md-form mb-0 labelprofile">
+											<span className="labelprofile">What service do you offer? </span>
+										</div>
+										<div className="file-upload-wrapper">
+											<input type="file" id="input-file-now" className="file-upload" />
+										</div>
+										</MDBCol> */}
+									</MDBRow>
+									<MDBRow>    
+										<MDBCol md="10 text-right">
+											<MDBBtn color="secondary">Submit</MDBBtn>
+										</MDBCol>
+									</MDBRow>
+								</MDBCardBody>
+							</MDBCol>				
+						</MDBRow>
+					</MDBCard>
+				</section>
+				</Container>	
+			);
+		};
+}
+export default Profile
