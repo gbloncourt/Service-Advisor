0c3d5be8c88684422a611851f1fc509985865d46
gbloncourt <54908400+gbloncourt@users.noreply.github.com>
2019-12-13 07:37:22 -0500

    Updates on 12 13 19
    Updates on 12 13 19

---
+++ src/components/profilecard.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class Profilecard extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            firstName : "",
+            lastName  : "",
+            serviceID : null
+        }       
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
